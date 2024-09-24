0a664fca3050c216623e23efc4d29bd325813c20
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-09-24 20:55:10 +0600

    initializing variables

--- main.c
+++ main.c
@@ -4,5 +4,12 @@
 #include <math.h>    
 
 int main() {
-   
+    int i = 1;
+    int n = 1000;
+    double I = 0.0;
+    double r;
+    double a = M_PI;             
+    double b = (3.0 * M_PI) / 2; 
+
+    
 }
